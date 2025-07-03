9d2fb73dbc74fd056e5fd1fba4eb41e81063defd
Julien Dehos <dehos@univ-littoral.fr>
2025-07-03 10:22:04 +0200

    up miso

--- src/Main.hs
+++ src/Main.hs
@@ -144,16 +144,11 @@ handleUpdate (ActionSetDuration sId t) =
 
 main :: IO ()
 main = run $ do
-
   let model = mkModel thePlaylist
-
-      app :: Component "app" Model Action
-      app = (defaultComponent model handleUpdate handleView)
-              { events = defaultEvents <> mediaEvents
-              , logLevel = DebugAll
-              }
-
-  startComponent app
+  startComponent (component model handleUpdate handleView)
+    { events = defaultEvents <> mediaEvents
+    , logLevel = DebugAll
+    }
 
 #ifdef WASM
 foreign export javascript "hs_start" main :: IO ()
