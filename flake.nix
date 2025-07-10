536b577886950853b2f84a897e3ce18f9ee00270
Julien Dehos <dehos@univ-littoral.fr>
2025-07-10 12:13:07 +0200

    package with flake
    error: builder for '/nix/store/5y62sncpqzpb5wz81lrji203l0lkrzy1-miso-audio.drv' failed with exit code 2;
           last 13 log lines:
           > Running phase: unpackPhase
           > unpacking source archive /nix/store/zhc88r6cnxyr34yqx4jvcplw251d5w8p-fa9lnkxarmdlgz7gi399lr51bxpfhr2l-source
           > source root is fa9lnkxarmdlgz7gi399lr51bxpfhr2l-source
           > Running phase: patchPhase
           > Running phase: updateAutotoolsGnuConfigScriptsPhase
           > Running phase: configurePhase
           > no configure script, doing nothing
           > Running phase: buildPhase
           > build flags: SHELL=/nix/store/00zrahbb32nzawrmv9sjxn36h7qk9vrs-bash-5.2p37/bin/bash
           > wasm32-wasi-cabal update
           > Cloning into '/build/fa9lnkxarmdlgz7gi399lr51bxpfhr2l-source/dist-newstyle/src/miso-7fdae9da4bdb552ef2daf21700284a28ca9d60f6abf96b737c89243d4bad27dc'...
           > fatal: unable to access 'https://github.com/dmjio/miso/': Could not resolve host: github.com
           > make: *** [Makefile:7: update] Error 128
           For full logs, run:
             nix log /nix/store/5y62sncpqzpb5wz81lrji203l0lkrzy1-miso-audio.drv

--- flake.nix
+++ flake.nix
@@ -5,9 +5,37 @@
   };
 
   outputs = inputs: 
-    inputs.miso.inputs.flake-utils.lib.eachDefaultSystem (system: {
-      devShell = inputs.miso.outputs.devShells.${system}.wasm;
-    });
+    inputs.miso.inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = inputs.miso.inputs.nixpkgs.legacyPackages.${system};
+
+      in rec {
+        devShell = inputs.miso.outputs.devShells.${system}.wasm;
+
+        packages = rec {
+          default = miso-audio;
+
+          miso-audio = pkgs.stdenv.mkDerivation {
+            name = "miso-audio";
+            src = ./.;
+            buildInputs = [
+              inputs.miso.inputs.ghc-wasm-meta.packages.${system}.all_9_12
+              pkgs.cabal-install
+              pkgs.git
+              pkgs.gnumake
+            ];
+            preBuild = ''
+              HOME=$PWD
+            '';
+            installPhase = ''
+              mkdir $out
+              cp -r public $out/
+            '';
+          };
+
+        };
+      }
+    );
 
 }
 
